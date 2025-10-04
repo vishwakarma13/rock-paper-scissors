faf414e61d958aa787e742c468ec319caa91c4fb
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-04 13:24:37 +0530

    add and initialize score tracking variables

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,15 +1,21 @@
 // Logic to get computer choice
+
 let compChoiceIndex;
-let choices = ["Rock", "Paper", "Scissors"]
+let choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice () {
     compChoiceIndex = Math.floor(Math.random()*3);
-    console.log(choices[compChoiceIndex])
+    console.log(choices[compChoiceIndex]);
 }
 
 // Logic to get human choice
 
-function gethumanchoice () {
-    let humanchoice= prompt("Write \"Stone\", \"Paper\" or \"Scissors\" (case sensitive)") 
-    console.log(humanchoice);
+function gethumanChoice () {
+    let humanChoice= prompt("Write \"Stone\", \"Paper\" or \"Scissors\" (case sensitive)");
+    console.log(humanChoice);
 }
+
+// Score tracking
+
+let humanScore =0;
+let computerScore =0;
