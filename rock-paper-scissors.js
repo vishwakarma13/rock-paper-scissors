87f5fb5e55ce2e6ea398bab1e6983f8f21643bde
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-04 13:12:51 +0530

    add function for humanchoice

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,3 +6,11 @@ function getComputerChoice () {
     compChoiceIndex = Math.floor(Math.random()*3);
     console.log(choices[compChoiceIndex])
 }
+
+// Logic to get human choice
+let humanchoiceindex;
+
+function gethumanchoice () {
+    let humanchoice= prompt("Write Stone, Paper or Scissors (case sensitive)") 
+    console.log(humanchoice);
+}
