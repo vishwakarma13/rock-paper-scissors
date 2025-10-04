b6ed974ca3a153136eed8a9c8c1a4fb1dd7e481f
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-05 03:27:14 +0530

    solve playround() not working bug

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -18,51 +18,47 @@ function getHumanChoice () {
 
 function playGame(){
    
-// Score tracking
-let humanScore =0;
-let computerScore =0;
+    // Score tracking
+    let humanScore =0;
+    let computerScore =0;
 
-//Logic for a single round
+    //Logic for a single round
 
-function playRound (humanChoice, computerChoice) {
+    function playRound (humanChoice, computerChoice) {
 
-    if (humanChoice===computerChoice) {
-    console.log("it's a tie");
-}
+        if (humanChoice===computerChoice) {
+        console.log("it's a tie");
+        console.log("No change in Player 1 score (human) -" +humanScore);
+        console.log("No change in Player 2 score (computer) -" +computerScore);
+    }
 
-else if (humanChoice==="paper" && computerChoice==="rock" || humanChoice==="rock" && computerChoice==="scissors" || humanChoice==="scissors" && computerChoice==="paper") 
-    {
-    console.log(`Player 1 (Human Wins), ${humanChoice} beats ${computerChoice} `)
-    return "Player 1 Winner";
-}
+    else if (humanChoice==="paper" && computerChoice==="rock" || humanChoice==="rock" && computerChoice==="scissors" || humanChoice==="scissors" && computerChoice==="paper") 
+        {
+        console.log(`Player 1 (Human Wins), ${humanChoice} beats ${computerChoice} `)
+        humanScore +=1;
+        console.log(humanScore);
+        console.log(computerScore);
+        return "Player 1 Winner";
+    }
 
-else {
-    console.log(`Player 2 (computer Wins), ${computerChoice} beats ${humanChoice} `)
-    return "Player 2 Winner";
-}
+    else {
+        console.log(`Player 2 (computer Wins), ${computerChoice} beats ${humanChoice} `)
+        computerScore +=1;
+        console.log(humanScore);
+        console.log(computerScore);
+        return "Player 2 Winner";
+        
+    }
 
-}
+    }
 
-const humanSelection = getHumanChoice().toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
+    const humanSelection = getHumanChoice().toLowerCase();
+    const computerSelection = getComputerChoice().toLowerCase();
 
+    playRound(humanSelection, computerSelection); //everytime playGame() runs it calls the playround function
 
 }
 
 for (let i=1; i<6; i++){
-playGame();
-    if (playRound()==="Player 1 Winner"){
-        humanScore +=1;
-        console.log(humanScore);
-        console.log(computerScore);
-    }
-    else if (playRound()==="Player 2 Winner"){
-        computerScore +=1;
-        console.log(humanScore);
-        console.log(computerScore);
-    }
-    else {
-        console.log("No change " +humanScore);
-        console.log("No change " +computerScore);
-    }
+    playGame();  // for logic is executed first according to which playGame() runs 5 times
 }
