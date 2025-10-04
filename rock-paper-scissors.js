eceadaa80830fbd58aff06daf7e8b4b60cf0019a
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-05 01:57:13 +0530

    add win lose and tie logic

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,21 +1,43 @@
 // Logic to get computer choice
 
-let compChoiceIndex;
-let computerChoice = ["Rock", "Paper", "Scissors"];
-
 function getComputerChoice () {
-    compChoiceIndex = Math.floor(Math.random()*3);
-    console.log(computerChoice[compChoiceIndex]);
+    let choices = ["Rock", "Paper", "Scissors"];
+    let compChoiceIndex = Math.floor(Math.random()*3);
+    let compResult = (choices[compChoiceIndex]);
+    return compResult;
 }
 
 // Logic to get human choice
 
-function gethumanChoice () {
-    let humanChoice= prompt("Write \"Stone\", \"Paper\" or \"Scissors\" (case sensitive)");
-    console.log(humanChoice);
+function getHumanChoice () {
+    let humRes= prompt("Write \"Stone\", \"Paper\" or \"Scissors\" (case sensitive)");
+    return humRes;
 }
 
 // Score tracking
 
 let humanScore =0;
 let computerScore =0;
+
+//Logic for a single round
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice===computerChoice) {
+    console.log("it's a tie");
+}
+else if (humanChoice==="Paper" && computerChoice==="Rock" || humanChoice==="Rock" && computerChoice==="Scissors" || humanChoice==="Scissors" && computerChoice==="Paper") 
+    {
+    console.log(`Player 1 (Human Wins) ${humanChoice} beats ${computerChoice}`)
+    return "Player 1 Winner";
+}
+
+else {
+    console.log(`Player 2 (computer Wins) ${computerChoice} beats ${humanChoice}`)
+    return "Player 2 Winner";
+}
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
