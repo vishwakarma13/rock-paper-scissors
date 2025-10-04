5eda8fbfecdfa2b1df34bf49f3ede1b2d32950af
Vaibhav Vishwakarma <vaibhav.vishwa1309@gmail.com>
2025-10-04 12:13:24 +0530

    add logic to generate the index and print the result

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,8 @@
 // Logic to get computer choice
 let compChoiceIndex;
 let choices = ["Rock", "Paper", "Scissors"]
-function getComputerChoice () {
 
+function getComputerChoice () {
+    compChoiceIndex = Math.floor(Math.random()*3);
+    console.log(choices[compChoiceIndex])
 }
